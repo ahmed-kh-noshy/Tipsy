a9e8638498c40378350dae621325baf5d29e77dd
ahmed khaled <ahmedkhaled5562.ak@gmail.com>
2023-08-04 00:46:05 +0300

    adding IBoutlet and IBactions and applying MVC

---
+++ Tipsy/Controller/ResultViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by ahmed khaled on 03/08/2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var totalLable: UILabel!
+    @IBOutlet weak var settingsLable: UILabel!
+    @IBAction func recalculateButton(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
