b1dcb6a66cf08756e747aaf5a873cf57aeee6e7a
ahmed khaled <ahmedkhaled5562.ak@gmail.com>
2023-08-04 14:36:14 +0300

    finishing up application

--- Tipsy/Controller/ResultViewController.swift
+++ Tipsy/Controller/ResultViewController.swift
@@ -9,14 +9,23 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+    
     @IBOutlet weak var totalLable: UILabel!
     @IBOutlet weak var settingsLable: UILabel!
+    
     @IBAction func recalculateButton(_ sender: UIButton) {
+        self.dismiss(animated: true)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        totalLable.text = result
+        settingsLable.text = "Split between \(split) people, with \(tip)% tip."
+
     }
     
 
